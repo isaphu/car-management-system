068c09e687368d55c2028fda289503ed68e7b9e5
isaphu <uronohan@gmail.com>
2021-04-12 14:40:51 +0700

    code formatting

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,124 +1,148 @@
-import React, { useState } from 'react';
-import Container from '@material-ui/core/Container';
-import TextFieldRow from './TextFieldRow';
-import IDUtils from '../util/id-util';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import SaveIcon from '@material-ui/icons/Save';
-import Button from '@material-ui/core/Button';
-
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import TextFieldRow from "./TextFieldRow";
+import IDUtils from "../util/id-util";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
 
 const useStyle = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1)
-        }
+  root: {
+    //put this to applied at 'root'
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
     },
-    button: {
-        margin: theme.spacing(1)
-    }
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
 function Car(_props) {
-    const classes = useStyle();
-    const [inputField, setInputField] = useState({ id: 0, licensePlate: '', carBrands: '', carModels: '', carRemarks: '' });
-    const [data, setData] = useState([]);
+  const classes = useStyle();
+  const [inputField, setInputField] = useState({
+    id: 0,
+    licensePlate: "",
+    carBrands: "",
+    carModels: "",
+    carRemarks: "",
+  });
+  const [data, setData] = useState([]);
 
-    const handleChange = (Id, event) => {
-        const index = data.findIndex(e => e.id === Id);
-        const newData = [...data];
-        newData[index][event.target.name] = event.target.value;
-        setData(newData);
+  const handleChange = (Id, event) => {
+    const index = data.findIndex((e) => e.id === Id);
+    const newData = [...data];
+    newData[index][event.target.name] = event.target.value;
+    setData(newData);
+  };
+
+  const handleChangeInput = (event) => {
+    const newInputField = { ...inputField };
+    newInputField[event.target.name] = event.target.value;
+    setInputField(newInputField);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("InputFields:", data);
+  };
+
+  const handleAddFields = () => {
+    const { licensePlate, carBrands, carModels, carRemarks } = inputField;
+    if (
+      [licensePlate, carBrands, carModels, carRemarks].every((e) => e === "")
+    ) {
+      return;
     }
+    setData([
+      ...data,
+      {
+        id: IDUtils.uniqueId(),
+        licensePlate,
+        carBrands,
+        carModels,
+        carRemarks,
+      },
+    ]);
+    setInputField({
+      licensePlate: "",
+      carBrands: "",
+      carModels: "",
+      carRemarks: "",
+    });
+  };
 
-    const handleChangeInput = (event) => {
-        const newInputField = {...inputField};
-        newInputField[event.target.name] = event.target.value;
-        setInputField(newInputField);
-    }
+  const handleRemoveFields = (id) => {
+    const newData = data.filter((e) => e.id !== id);
+    setData(newData);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("InputFields:", data)
-    }
-
-    const handleAddFields = () => {
-        const { licensePlate, carBrands, carModels, carRemarks } = inputField;
-        if ([licensePlate, carBrands, carModels, carRemarks].every(e => e === "")) {
-            return; 
-        }
-        setData([...data, { id: IDUtils.uniqueId(), licensePlate, carBrands, carModels, carRemarks }])
-        setInputField({ licensePlate: '', carBrands: '', carModels: '', carRemarks: ''});
-    }
-
-    const handleRemoveFields = (id) => {
-        const newData = data.filter(e => e.id !== id);
-        setData(newData);
-    }
-
-    return (
-        <Container>
-            <div className='car-form-wrap'>
-                <h1 style={{ color: 'black', fontFamily: 'sans-serif' }}>🚘 Car Management System</h1>
-                <form className={classes.root}>
-                    <TextField
-                        variant="outlined"
-                        size='small'
-                        name='licensePlate'
-                        label='License Plates'
-                        value={inputField.licensePlate}
-                        onChange={event => handleChangeInput(event)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        size='small'
-                        name='carBrands'
-                        label='Brands'
-                        value={inputField.carBrands}
-                        onChange={event => handleChangeInput(event)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        size='small'
-                        name='carModels'
-                        label='Models'
-                        value={inputField.carModels}
-                        onChange={event => handleChangeInput(event)}
-                    />
-                    <TextField
-                        id='remark-box'
-                        variant="outlined"
-                        size='small'
-                        name='carRemarks'
-                        label='Remarks'
-                        width='100ch'
-                        value={inputField.carRemarks}
-                        onChange={event => handleChangeInput(event)}
-                    />
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        className={classes.button}
-                        startIcon={<SaveIcon />}
-                        onClick={() => handleAddFields()}
-                        >
-                            Save
-                    </Button>
-                    {data.map((carInfo, _index) => (
-                        <TextFieldRow
-                            key={carInfo.id}
-                            carInfo={carInfo}
-                            handleRemoveFields={handleRemoveFields}
-                            handleAddFields={handleAddFields}
-                            handleChange={handleChange}
-                            handleSubmit={handleSubmit}
-                        />
-                    ))}
-                </form>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <div className="car-form-wrap">
+        <h1 style={{ color: "black", fontFamily: "sans-serif" }}>
+          🚘 Car Management System
+        </h1>
+        <form className={classes.root}>
+          <TextField
+            variant="outlined"
+            size="small"
+            name="licensePlate"
+            label="License Plates"
+            value={inputField.licensePlate}
+            onChange={(event) => handleChangeInput(event)}
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            name="carBrands"
+            label="Brands"
+            value={inputField.carBrands}
+            onChange={(event) => handleChangeInput(event)}
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            name="carModels"
+            label="Models"
+            value={inputField.carModels}
+            onChange={(event) => handleChangeInput(event)}
+          />
+          <TextField
+            id="remark-box"
+            variant="outlined"
+            size="small"
+            name="carRemarks"
+            label="Remarks"
+            width="100ch"
+            value={inputField.carRemarks}
+            onChange={(event) => handleChangeInput(event)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            className={classes.button}
+            startIcon={<SaveIcon />}
+            onClick={() => handleAddFields()}
+          >
+            Save
+          </Button>
+          {data.map((carInfo, _index) => (
+            <TextFieldRow
+              key={carInfo.id}
+              carInfo={carInfo}
+              handleRemoveFields={handleRemoveFields}
+              handleAddFields={handleAddFields}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
+          ))}
+        </form>
+      </div>
+    </Container>
+  );
 }
 
 export default Car;
--- src/components/TextFieldRow.js
+++ src/components/TextFieldRow.js
@@ -1,110 +1,102 @@
-import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import SaveIcon from '@material-ui/icons/Save';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { makeStyles } from '@material-ui/core';
-import EditIcon from '@material-ui/icons/Edit';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
 
 const useStyle = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1)
-        }
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
     },
-    button: {
-        margin: theme.spacing(1)
-    }
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
 function TextFieldRow(props) {
-    const [isEdit, setIsEdit] = useState(false);
-    const classes = useStyle();
-    const {
-        carInfo,
-        handleRemoveFields,
-        handleChange,
-    } = props;
+  const [isEdit, setIsEdit] = useState(false);
+  const classes = useStyle();
+  const { carInfo, handleRemoveFields, handleChange } = props;
 
-    return (
-        <div key={carInfo.id}>
-            <TextField
-                disabled={!isEdit}
-                size='small'
-                variant="outlined"
-                name='licensePlate'
-                label='License Plates'
-                value={carInfo.licensePlate}
-                onChange={event => handleChange(carInfo.id, event)}
-            />
-            <TextField
-                disabled={!isEdit}
-                size='small'
-                variant="outlined"
-                name='carBrands'
-                label='Brands'
-                value={carInfo.carBrands}
-                onChange={event => handleChange(carInfo.id, event)}
-
-            />
-            <TextField
-                disabled={!isEdit}
-                size='small'
-                variant="outlined"
-                name='carModels'
-                label='Models'
-                value={carInfo.carModels}
-                onChange={event => handleChange(carInfo.id, event)}
-
-            />
-            <TextField
-                disabled={!isEdit}
-                size='small'
-                id='remark-box'
-                variant="outlined"
-                name='carRemarks'
-                label='Remarks'
-                width='100ch'
-                value={carInfo.carRemarks}
-                onChange={event => handleChange(carInfo.id, event)}
-            />
-            {isEdit ?
-                (
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        className={classes.button}
-                        startIcon={<SaveIcon />}
-                        onClick={() => setIsEdit(false)}
-                    >
-                        Save
-                    </Button>
-                ) : (
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        className={classes.button}
-                        startIcon={<EditIcon />}
-                        onClick={() => setIsEdit(true)}
-                    >
-                        Edit
-                    </Button>
-                )
-            }
-            <Button
-                variant="contained"
-                color="primary"
-                size="small"
-                className={classes.button}
-                startIcon={<DeleteIcon />}            
-                onClick={() => handleRemoveFields(carInfo.id)}
-                >
-                    Delete
-            </Button>
-        </div>
-    )
+  return (
+    <div key={carInfo.id}>
+      <TextField
+        disabled={!isEdit}
+        size="small"
+        variant="outlined"
+        name="licensePlate"
+        label="License Plates"
+        value={carInfo.licensePlate}
+        onChange={(event) => handleChange(carInfo.id, event)}
+      />
+      <TextField
+        disabled={!isEdit}
+        size="small"
+        variant="outlined"
+        name="carBrands"
+        label="Brands"
+        value={carInfo.carBrands}
+        onChange={(event) => handleChange(carInfo.id, event)}
+      />
+      <TextField
+        disabled={!isEdit}
+        size="small"
+        variant="outlined"
+        name="carModels"
+        label="Models"
+        value={carInfo.carModels}
+        onChange={(event) => handleChange(carInfo.id, event)}
+      />
+      <TextField
+        disabled={!isEdit}
+        size="small"
+        id="remark-box"
+        variant="outlined"
+        name="carRemarks"
+        label="Remarks"
+        width="100ch"
+        value={carInfo.carRemarks}
+        onChange={(event) => handleChange(carInfo.id, event)}
+      />
+      {isEdit ? (
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          className={classes.button}
+          startIcon={<SaveIcon />}
+          onClick={() => setIsEdit(false)}
+        >
+          Save
+        </Button>
+      ) : (
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          className={classes.button}
+          startIcon={<EditIcon />}
+          onClick={() => setIsEdit(true)}
+        >
+          Edit
+        </Button>
+      )}
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+        onClick={() => handleRemoveFields(carInfo.id)}
+      >
+        Delete
+      </Button>
+    </div>
+  );
 }
 
 export default TextFieldRow;
